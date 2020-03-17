773598e94003ae9d1993f67b9995d955368d872c
danielgithub-pullrequest <62295789+danielgithub-pullrequest@users.noreply.github.com>
2020-03-17 09:44:21 -0500

    First commit setup

---
+++ javaScript-test.js
@@ -0,0 +1,19 @@
+
+const fs = require('fs');
+const readline = require('readline');
+
+console.log('helloooooo');
+
+var x = 12;
+
+if (x===-0){
+  console.log("Hello");
+}
+
+while(true){
+}
+
+
+console.log("A");
+debugger;
+
